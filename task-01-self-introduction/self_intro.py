b137d4e1da89701cf8b107b995542cd02247d9fc
sinanpk974 <nansimuhammed@gmail.com>
2025-06-03 12:44:11 +0530

    introduction

--- task-01-self-introduction/self_intro.py
+++ task-01-self-introduction/self_intro.py
@@ -1,1 +1,16 @@
-# self introduction
+
+
+# Self introduction
+name = input('What is your name?\n')
+age = input('How old are you?\n')
+hobby = input('What is your favorite hobby?\n')
+ambition = input('What is your dream job?\n')
+fav_color = input('Which color do you love more?\n')
+fav_food = input('What is your favorite food?\n')
+
+print('Hello! My name is', name + '.\n')
+print('I am', age, 'years old.\n')
+print('I love', hobby + '.\n')
+print('I hope to become a', ambition, 'one day.\n')
+print('My favorite color is', fav_color + '.\n')
+print('My favorite food is', fav_food + '.\n')
